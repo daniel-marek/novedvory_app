58af2d8e3fcd2d7561c1b2e083a233a8b224a918
Plamic14 <danielmarek66@gmail.com>
2023-01-25 18:09:44 +0100

    clanky upraveny + sekce a linky

--- components/Topbar.js
+++ components/Topbar.js
@@ -24,21 +24,21 @@ const Topbar = () => {
 
     return (
         <header className={styles.header}>
-          <div className={styles.container__header}>
+          <div className={`${styles.container__header} ${styles.container}`}>
             <a className={styles.logo} href="#">
             <img className={styles.logo__img}  src="logo.png" alt="logo-nove-dvory" />
             </a>
             <nav>
               <ul className={styles.nav__links}>
-                <li><a href="#">Aktuality</a></li>
-                <li><a href="#">Muži A</a></li>
-                <li><a href="#">Hřiště</a></li>
-                <li><a href="#">Sponzoři</a></li>
-                <li><a href="#">Historie</a></li>
-                <li><a href="#">Klub</a></li>
+                <li><a href="#aktuality">Aktuality</a></li>
+                <li><a href="#sponzori">Sponzoři</a></li>
+                <li><a href="#klub">Klub</a></li>
+                <li><a href="#hriste">Hřiště</a></li>
+                <li><a href="#historie">Historie</a></li>
+                
               </ul>
             </nav>
-            <a className={styles.cta} href="#">
+            <a className={styles.cta} href="https://is.fotbal.cz/souteze/detail-souteze.aspx?req=9767b870-a9eb-4444-bec0-01acaae098c9" target="_blank">
                 <button>Program zápasů</button>
             </a>
             <div className={styles.hamburger} >
