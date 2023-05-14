118aa35a116ad4cf9834b733ca33fd9d7c5ba479
Jan Stepnicka <honzastepnicka@gmail.com>
2023-05-14 20:59:22 +0200

    fix logo

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import styles from '../styles/Home.module.scss'
 import { fetchArticlesForIndex } from '../data'
 import useSwr from 'swr'
 import React from 'react'
+import Image from 'next/image'
 
 export default function Home() {
   const { data: articles } = useSwr('articles', fetchArticlesForIndex)
@@ -153,10 +154,11 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <div className={styles.footer__items}>
-          <a href="#">
-            <img
-              className={styles.logo__img}
-              src="logo.png"
+          <a href="/" className={styles.logo}>
+            <Image
+              src="/logo.png"
+              width="100%"
+              height="100%"
               alt="logo-nove-dvory"
             />
           </a>
